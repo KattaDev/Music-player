19d0c9bd5ce28ebe636712ffba10ae88c65cf421
KattaDev <ulugbekrasmiy1999@gamil.com>
2022-01-04 12:14:27 +0500

    login page ui

--- lib/pages/loginpage/login_page.dart
+++ lib/pages/loginpage/login_page.dart
@@ -13,23 +13,23 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage("assets/images/loginback.png"))),
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height*1.2,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/loginback.png"))),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Container(
                 margin: EdgeInsets.only(
-                    top: getH(240),
+                    top: getH(200),
                     left: getW(175),
                     right: getW(175),
-                    bottom: 160),
+                    bottom: 150),
                 height: getH(200),
                 width: getW(400),
                 child: SvgPicture.asset('assets/icons/uilogo.svg',
@@ -47,7 +47,7 @@ class LoginPage extends StatelessWidget {
                         blur: 20,
                         shadowStrength: 20,
                         opacity: 0.2,
-
+      
                         //--code to remove border
                         border: Border.fromBorderSide(BorderSide.none),
                         borderRadius: BorderRadius.circular(10),
@@ -89,42 +89,52 @@ class LoginPage extends StatelessWidget {
                           ),
                         ),
                       ),
-                      SizedBox(
+                     GlassContainer(
+                        blur: 20,
+                        shadowStrength: 20,
+                        opacity: 0.2,
+      
+                        //--code to remove border
+                        border: Border.fromBorderSide(BorderSide.none),
+                        borderRadius: BorderRadius.circular(10),
+                        child: SizedBox(
                         height: getH(110),
                         child: TextFormField(
-                          style: TextStyle(
-                              color: whitecolor, fontWeight: FontWeight.w500),
-                          controller: _passwordcontroller,
-                          decoration: InputDecoration(
-                              suffix: TextButton(
-                                child: Text(
-                                  "SHOW",
-                                  style: TextStyle(color: whitecolor),
-                                ),
-                                onPressed: () {},
-                                style: TextButton.styleFrom(
-                                    backgroundColor: Colors.blueGrey,
-                                    maximumSize: Size(getW(160), getH(80)),
-                                    fixedSize: Size(getW(160), getH(60))),
+                        style: TextStyle(
+                            color: whitecolor, fontWeight: FontWeight.w500),
+                        controller: _passwordcontroller,
+                        decoration: InputDecoration(
+                            suffix: TextButton(
+                              child: Text(
+                                "SHOW",
+                                style: TextStyle(color: whitecolor),
                               ),
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(width: 0)),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(
-                                    getW(8),
-                                  ),
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                  backgroundColor: Colors.blueGrey,
+                                  maximumSize: Size(getW(160), getH(80)),
+                                  fixedSize: Size(getW(160), getH(60))),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 0)),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(
+                                  getW(8),
                                 ),
                               ),
-                              hintText: "Your password",
-                              filled: true,
-                              fillColor: inputcolor),
+                            ),
+                            hintText: "Your password",
+                           
+                            ),
                         ),
-                      ),
+                      ))
                     ],
                   ),
                 ),
               ),
+              SizedBox(height: getH(50),),
+      
               ElevatedButton(
                 onPressed: () {},
                 child: Text("SIGN IN"),
@@ -136,14 +146,15 @@ class LoginPage extends StatelessWidget {
                           BorderRadius.all(Radius.circular(getW(50)))),
                 ),
               ),
+              SizedBox(height: getH(50),),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   SizedBox(
-                    width: getW(100),
+                    width: getW(150),
                     child: Divider(
                       height: 2,
-                      thickness: 5,
+                      thickness: 2,
                       color: Colors.blueGrey
                     ),
                   ),
@@ -152,26 +163,29 @@ class LoginPage extends StatelessWidget {
                     child: Text("OR",style: TextStyle(color: whitecolor,fontWeight: FontWeight.w500),),
                   ),
                  SizedBox(
-                    width: getW(100),
+                    width: getW(150),
                     child: Divider(
                       height: 2,
-                      thickness: 5,
+                      thickness: 2,
                       color: Colors.blueGrey
                     ),
                   ),
                 ],
               ),
+              SizedBox(height: getH(50),),
+      
                GlassContainer(
                         blur: 2,
                         shadowStrength: 20,
                         opacity: 0.2,
-
+      
                         //--code to remove border
                         border: Border.fromBorderSide(BorderSide.none),
                         borderRadius: BorderRadius.circular(50),
                         child:ElevatedButton(
                 onPressed: () {},
-                child: Text("SIGN IN"),
+                child:  SvgPicture.asset('assets/icons/facebook.svg',
+                    color: whitecolor),
                 style: ElevatedButton.styleFrom(
                   primary: Colors.transparent,
                   fixedSize: Size(getW(630), getH(110)),
@@ -179,7 +193,9 @@ class LoginPage extends StatelessWidget {
                       borderRadius:
                           BorderRadius.all(Radius.circular(getW(50)))),
                 ),
-              ),)
+              ),),
+              SizedBox(height: getH(50),),
+      Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,children: [Text("Forgot details?",style: TextStyle(color: Colors.grey),),Text("Create Account",style: TextStyle(color: Colors.grey),),],)
             ],
           ),
         ),
